67962e26e1bcfe9f263b890542ecd752a03af82b
Vishwesh Jirgale <vijirgal@microsoft.com>
2016-01-02 13:50:00 +0530

    Week 1 Excercise 3

---
+++ excercise3/constants.go
@@ -0,0 +1,26 @@
+// Exercise 3 - Two problems on Constants
+//
+// 1. Declare an untyped and typed constant and display their values.
+//
+// 2.Multiply two literal constants into a typed variable and display
+// the values of both the constants and variables.
+
+package main
+
+import "fmt"
+
+// main is the entry point for the application.
+func main() {
+	// Declare an untyped and typed constant.
+	const hello = "Hello"
+	const typedHello string = "世界"
+
+	// Display the value of those variables.
+	fmt.Println(hello)
+	fmt.Println(typedHello)
+
+	// Multiply two literal constants into a typed variable
+	seconds := 60 * 60
+	// Display the value of those variables.
+	fmt.Println(seconds)
+}
